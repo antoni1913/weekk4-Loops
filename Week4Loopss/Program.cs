ade47076283173b6c6c1a0b7a907c080bf9f649a
antoni1913 <antonisariev13@gmail.com>
2023-10-17 00:36:00 +0300

    weekk4

---
+++ Week4Loopss/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Random rand = new Random();
+
+
+
+for (int i = 0; i < 32; i++)
+{
+    int myRadom = rand.Next(1, 11);
+
+    Console.WriteLine(myRadom);
+}
+
+
+//for(int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"{i + 1}i will not skill clasees");
+//}
---
+++ Week4WileLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+Random rnd = new Random();
+
+int cpuRandom = rnd.Next(1, 4);
+
+bool loopActive = true;
+
+int numberOfTrue = 0;
+
+while (numberOfTrue < 3)
+{
+    Console.WriteLine("Try to guess the cpu number:");
+    int userGuess = Convert.ToInt32(Console.ReadLine());
+
+    numberOfTrue++;
+
+    if (userGuess == cpuRandom)
+    {
+        Console.WriteLine("YOU WON Iphone15 :D");
+        break;
+    }
+
+}
+Console.WriteLine($"number of trues: {numberOfTrue}");
+Console.WriteLine("Have a nice day");
